023faa93f4ebe2d810f3935d6b8be06bc7fdae7b
Elias Lieber <eliasliebermail@gmail.com>
2023-05-11 20:49:21 +0200

    y

---
+++ src/constants.py
@@ -0,0 +1,24 @@
+# Path
+PATH = "/home/elias/Dev/"
+
+# Screen dimensions
+HEIGHT = 1200
+WIDTH = 1200
+
+# Board dimensions
+ROWS = 8
+COLS = 8
+SQSIZE = WIDTH // COLS
+
+# Game Information
+FPS = 144
+
+# options
+SCREEN_DIMENSIONS = [(WIDTH, HEIGHT)]
+
+# Singleplayer
+STOCKFISH_ELO = 1800
+
+# Multiplayer
+SERVER_IP = "localhost"
+PORT = 5555
